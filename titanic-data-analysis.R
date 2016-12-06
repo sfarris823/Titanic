f140be9941d306988d8b44c453475e16eb758159
Scott J Farris <sfarris823@gmail.com>
2016-12-06 17:23:29 -0600

    Add random forest and modeling capability

--- titanic-data-analysis.R
+++ titanic-data-analysis.R
@@ -2,8 +2,13 @@
 rm(list=ls())
 
 # load needed libraries
-library(dplyr)
-library(ggplot2)
+library(dplyr)           #data manipulation
+library(ggplot2)         #simple plots
+library(randomForest)    #exploratory modeling
+library(caret)           #cross validation
+library(doSNOW)          #clustering for performance
+library(rpart)
+library(rpart.plot)
 
 ##
 ## Custom functions
@@ -32,6 +37,14 @@ showhist <- function(data, xaxis, fillgrp, facetwrap, title)
     ylab("Total Count") +
     labs(fill = fillgrp)
 }
+
+rpart.cv <- function(seed, training, labels, ctrl)
+{
+  set.seed(seed)
+  rpart.cv <- train(x=training, y=labels, method="rpart", tuneLength=30, trControl=ctrl)
+  return (rpart.cv)
+}
+
 
 ##
 ## Get data
@@ -87,7 +100,24 @@ combined$SimpleTitle <- as.factor(combined$SimpleTitle)
 combined$FamilySize <- as.numeric(paste(combined$SibSp)) + as.numeric(paste(combined$Parch)) + 1
 combined$FamilySize <- as.factor(combined$FamilySize)
 
+# Adjust Fare based on number of passengers per ticket
+ticket.party.size <- rep(0, nrow(combined))
+avg.fare <- rep(0.0, nrow(combined))
+tickets <- unique(combined$Ticket)
 
+for (i in 1:length(tickets)) {
+  current.ticket <- tickets[i]
+  party.indexes <- which(combined$Ticket == current.ticket)
+  current.avg.fare <- combined[party.indexes[1], "Fare"] / length(party.indexes)
+  
+  for (k in 1:length(party.indexes)) {
+    ticket.party.size[party.indexes[k]] <- length(party.indexes)
+    avg.fare[party.indexes[k]] <- current.avg.fare
+  }
+}
+
+combined$PartySize <- ticket.party.size
+combined$AverageFare <- avg.fare
 
 ##
 ##  Explore the data with plots
@@ -127,3 +157,94 @@ showhist(train.adj, "FamilySize", "Survived", ~Pclass+SimpleTitle, "Survival Rat
 table(filter(select(combined, Pclass, Cabin), Pclass=="1"))
 table(filter(select(combined, Pclass, Cabin), Pclass=="2"))
 table(filter(select(combined, Pclass, Cabin), Pclass=="3"))
+
+
+# Exploratory Modeling
+set.seed(1234)
+
+rf.1 <- randomForest(x = combined[1:891, c("Pclass", "SimpleTitle", "FamilySize")], 
+                     y = as.factor(train$Survived), 
+                     importance = T, 
+                     ntree = 500)
+rf.1
+varImpPlot(rf.1)
+
+rf.2 <- randomForest(x = combined[1:891, c("Pclass", "SimpleTitle", "PartySize", "AverageFare")], 
+                     y = as.factor(train$Survived), 
+                     importance = T, 
+                     ntree = 500)
+rf.2
+varImpPlot(rf.2)
+
+# Cross Validation
+set.seed(37596)
+rf.label <- as.factor(train$Survived)
+cv.10.folds <- createMultiFolds(rf.label, k = 3, times = 10)
+
+
+ctrl.1 <- trainControl(method = "repeatedcv", number = 3, repeats = 10, index = cv.10.folds)
+
+# not clustering due to PC power.
+#mycluster <- makeCluster(2, type="SOCK")
+#registerDoSNOW(mycluster)
+
+set.seed(94622)
+rf.1.cv.1 <- train(x=combined[1:891, c("Pclass", "SimpleTitle", "FamilySize")], 
+                   y=rf.label, 
+                   method="rf",
+                   tuneLength=3, 
+                   ntree=64, 
+                   trControl=ctrl.1)
+
+# not clustering due to PC power.
+#stopCluster(mycluster)
+rf.1.cv.1
+
+set.seed(94622)
+rf.2.cv.1 <- train(x=combined[1:891, c("Pclass", "SimpleTitle", "PartySize", "AverageFare")], 
+                   y=rf.label, 
+                   method="rf",
+                   tuneLength=3, 
+                   ntree=64, 
+                   trControl=ctrl.1)
+
+# not clustering due to PC power.
+#stopCluster(mycluster)
+rf.2.cv.1
+
+# More Exploratory Modeling
+
+features <- c("Pclass", "SimpleTitle", "FamilySize")
+rpart.train.1 <- combined[1:891, features]
+rpart.1.cv.1 <- rpart.cv(94622, rpart.train.1, rf.label, ctrl.1)
+rpart.1.cv.1
+prp(rpart.1.cv.1$finalModel, type=0, extra=1, under=T)
+rpart.1.cv.1$finalModel
+# The plot brings out some interesting lines of investigation. Namely:
+#      1 - SimpleTitles of "Mr" are predicted to perish at an
+#          overall accuracy rate of 80.93%
+#      2 - SimpleTitles of "Master", "Miss", and "Mrs" in 1st and 2nd class
+#          are predicted to survive at an overall accuracy rate of 94.8%
+#      3 - SimpleTitles of "Master", "Miss", and "Mrs" in 3rd class with
+#          family sizes equal to 5, 6, 8, and 11 are predicted to perish
+#          with 100% accuracy. [this may be overfitting due to specificity]
+#      4 - SimpleTitles of "Master", "Miss", and "Mrs" in 3rd class with
+#          family sizes not equal to 5, 6, 8, and 11 are predicted to survive
+#          with 66.27% accuracy.
+
+rpart.train.2 <- combined[1:891, c("Pclass", "SimpleTitle", "PartySize", "AverageFare")]
+rpart.2.cv.1 <- rpart.cv(94622, rpart.train.2, rf.label, ctrl.1)
+rpart.2.cv.1
+prp(rpart.2.cv.1$finalModel, type=0, extra=1, under=T)
+rpart.2.cv.1$finalModel
+# The plot brings out some interesting lines of investigation. Namely:
+#      1 - SimpleTitles of "Mr" are predicted to perish at an
+#          overall accuracy rate of 80.93%
+#      2 - SimpleTitles of "Master", "Miss", and "Mrs" in 1st and 2nd class
+#          are predicted to survive at an overall accuracy rate of 94.8%
+#      3 - SimpleTitles of "Master", "Miss", and "Mrs" in 3rd class with
+#          a party size greater than 4.5 are predicted to perish
+#          with 92.68% accuracy.
+#      4 - SimpleTitles of "Master", "Miss", and "Mrs" in 3rd class with
+#          a party size greater than 4.5 and an average fare greater than or 
+#          equal to 8 are predicted to survive with 58.9% accuracy.
